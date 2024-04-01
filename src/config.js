dfdfa5a09c03e1b4dc0e328a193c9c9028a8f8fa
neelra8n <neelratn.official@gmail.com>
2024-04-01 22:39:16 +0530

    proxy

--- src/config.js
+++ src/config.js
@@ -2,7 +2,7 @@ export const BASE_URL =
   "https://res.cloudinary.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_508,h_320,c_fill/";
 
 export const FETCH_MENU_URL =
-  "https://www.swiggy.com/dapi/menu/pl?page-type=REGULAR_MENU&complete-menu=true&lat=22.5135084&lng=88.402884&restaurantId=";
+  "dapi/menu/pl?page-type=REGULAR_MENU&complete-menu=true&lat=22.5135084&lng=88.402884&restaurantId=";
 
 export const RestaurantList = [
   {
